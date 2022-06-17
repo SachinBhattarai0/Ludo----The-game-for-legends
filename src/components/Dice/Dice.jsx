11ad74e25a69645519a503aa7c0ae526426306ed
SachinBhattarai0 <shachinbh023@gmail.com>
2022-06-17 19:25:31 +0545

    Added context containing game infos

--- src/components/Dice/Dice.jsx
+++ src/components/Dice/Dice.jsx
@@ -1,18 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useGameInfo } from "../../context/GameInfoContext";
 // Used modular form to practice
 import classes from "./Dice.module.css";
 
 const Dice = () => {
+  const { GameInfo, setGameInfo } = useGameInfo();
+  const { currentUserPoints } = GameInfo;
+
+  const generateNewRandomPoints = () => {
+    const randomPoints = Math.round(Math.random() * 6);
+    setGameInfo({ ...GameInfo, ["currentUserPoints"]: randomPoints });
+  };
+
   return (
-    <div className={classes["dice-container"]}>
+    <div
+      className={classes["dice-container"]}
+      onClick={generateNewRandomPoints}
+    >
       <p>Red's Turn</p>
       <div className={classes.dice}>
-        <div className={classes["dice-dot"]}></div>
-        <div className={classes["dice-dot"]}></div>
-        <div className={classes["dice-dot"]}></div>
-        <div className={classes["dice-dot"]}></div>
-        <div className={classes["dice-dot"]}></div>
-        <div className={classes["dice-dot"]}></div>
+        {/* {new Array(currentUserPoints).map((_,i) => {
+          return <div key={i} className={classes["dice-dot"]}></div>;
+        })} */}
       </div>
     </div>
   );
---
+++ src/context/GameInfoContext.jsx
@@ -0,0 +1,74 @@
+import React, { useContext, createContext, useState } from "react";
+
+const TokenContext = createContext();
+
+class TokensClass {
+  constructor(Color, NoOfHomes, token1, token2, token3, token4, parents) {
+    this.Color = Color;
+    this.NoOfHomes = NoOfHomes;
+    this.token1 = token1;
+    this.token2 = token2;
+    this.token3 = token3;
+    this.token4 = token4;
+    this.parents = parents;
+  }
+  // Methods
+}
+
+let red = new TokensClass(
+  "red",
+  0,
+  null,
+  null,
+  null,
+  null,
+  document.querySelectorAll(".red .innerp2")
+);
+let green = new TokensClass(
+  "green",
+  0,
+  null,
+  null,
+  null,
+  null,
+  document.querySelectorAll(".green .innerp2")
+);
+let yellow = new TokensClass(
+  "yellow",
+  0,
+  null,
+  null,
+  null,
+  null,
+  document.querySelectorAll(".yellow .innerp2")
+);
+let blue = new TokensClass(
+  "blue",
+  0,
+  null,
+  null,
+  null,
+  null,
+  document.querySelectorAll(".blue .innerp2")
+);
+
+
+
+const GameInfoContextProvider = ({ children }) => {
+  // const [currentUserPoints,setCurrentUserPoints] = useState(null)//To store current point obtained by user
+
+  const [GameInfo, setGameInfo] = useState({
+    turn: "red",
+    tokenInfo: { red, green, yellow, blue },
+    currentUserPoints:0,
+  });
+
+  return (
+    <TokenContext.Provider value={{ GameInfo, setGameInfo }}>
+      {children}
+    </TokenContext.Provider>
+  );
+};
+
+export const useGameInfo = () => useContext(TokenContext);
+export default GameInfoContextProvider;
